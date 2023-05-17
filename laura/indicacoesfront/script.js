db0dc66d933a2896a58412bf607a83c2a55f9b93
LauraRickes <132913067+LauraRickes@users.noreply.github.com>
2023-05-17 11:57:06 -0300

    atualizar

---
+++ laura/indicacoesfront/script.js
@@ -0,0 +1,8 @@
+const greetingMenssage = () => 
+ let  = new Date(). getHours();
+ if (h <=5) return 'Boa madrugada';
+ if (h< 12) return 'Bom diaa';
+ if (h<18) return 'Boa tarde';
+  return 'Boa noite';
+
+document.getElementById("mensagem").innerHTML =(greetingMenssage());
