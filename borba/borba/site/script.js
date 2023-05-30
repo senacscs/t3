2c2d37a7adfb0b10de680e1ade1042ec0b468e2f
Davi-Borba <132914458+Davi-Borba@users.noreply.github.com>
2023-05-30 09:08:12 -0300

    subir borba

---
+++ borba/borba/site/script.js
@@ -0,0 +1,11 @@
+ alert("Bem vindo ao meu site");
+ console.log("The Walking Dead");
+ const greetingMessage =() => {
+	let h = new Date(). getHours();
+	console.log(h);
+	if (h <= 5) return 'Boa madrugada';
+	if (h < 12) return 'Bom dia';
+	if (h < 18) return 'Boa tarde';
+	return 'Boa tarde';
+}
+ document.getElementById("demo").innerHTML = (greetingMessage());
