09d3064300b10b2c19f4f75cbbe8fe4b1fb0524e
HenriqueArturDefaveri <132913054+HenriqueArturDefaveri@users.noreply.github.com>
2023-06-29 08:41:43 -0300

    Add files via upload

---
+++ henrique/carrinho/script.js
@@ -0,0 +1,15 @@
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrossel(){
+    idx++;
+
+    if(idx > img.length - 1){
+        idx = 0;
+    }
+
+    imgs.style.transform = `translateX(${-idx * 400}px)`;
+}
+setInterval (carrossel, 1800);
