4639e0f92a529859ad0c66c56824857101cce387
soso <132917029+sophiascholante@users.noreply.github.com>
2023-07-06 08:50:16 -0300

    att

--- sophia/script.js
+++ sophia/script.js
@@ -1,8 +1,8 @@
 const greetingMessage = () => {
 let h = new Date().getHours();
-if (h <= 5) return 'ALERTA DE GATILHO:  Não me responsabilizo pelas feias que se magoarem com o conteúdo disponível neste site';
-if (h < 12) return 'ALERTA DE GATILHO:  Não me responsabilizo pelas feias que se magoarem com o conteúdo disponível neste site';
-if (h < 18) return 'ALERTA DE GATILHO:  Não me responsabilizo pelas feias que se magoarem com o conteúdo disponível neste site';
+if (h <= 5) return 'ALERTA DE GATILHO:  Não me responsabilizo pelas feias que se magoarem com o conteúdo disponível neste site!! Ninguém mandou nascer feia desse jeito;';
+if (h < 12) return 'ALERTA DE GATILHO:  Não me responsabilizo pelas feias que se magoarem com o conteúdo disponível neste site!! Ninguém mandou nascer feia desse jeito;';
+if (h < 18) return 'ALERTA DE GATILHO:  Não me responsabilizo pelas feias que se magoarem com o conteúdo disponível neste site!! Ninguém mandou nascer feia desse jeito;';
 }
 alert(greetingMessage());
 
