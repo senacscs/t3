c157ca6f7aedcdf6e56083ca291b1e0f804b64dd
brunohensel08 <132918729+brunohensel08@users.noreply.github.com>
2023-09-04 20:13:06 -0300

    up

--- bruno/micmusic/login/assets/js/index.js
+++ bruno/micmusic/login/assets/js/index.js
@@ -1,3 +1,31 @@
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=>
+    item.addEventListener('click', selectLink)
+)
+
+//Expandir o menu
+
+var btnExp = document.querySelector('#btn-exp')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
+
+
+
+
+
+
+
 if(localStorage.getItem('token') == null){
     alert('Você precisa estar logado para acessar essa página')
     window.location.href = "./assets/html/signin.html"
