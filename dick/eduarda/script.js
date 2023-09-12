3e9f117a77031dba2ffa2d597e3d036f500e0243
EduLunardi0130 <132913101+EduLunardi0130@users.noreply.github.com>
2023-09-12 11:33:13 -0300

    ajustes

--- dick/eduarda/script.js
+++
@@ -1,8 +0,0 @@
-const greetingMessage = () => { 
-	let h = new Date().getHours();  
-	if (h <= 5) return 'Boa madrugada';
-	if (h < 12) return 'Bom dia';
-	if (h < 18) return 'Boa tarde';
- 	return 'Boa noite'; 
-}  
-document.getElementById("mensagem").innerHTML =(greetingMessage());
