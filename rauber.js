ab0ce79aa23dac2d9be93318e12511c9e0e4a9c8
Rodrigo Dini <83221997+Rodrik227@users.noreply.github.com>
2023-09-18 10:22:21 -0300

    added rauber

---
+++ rauber.js
@@ -0,0 +1,8 @@
+function sim() {
+    document.getElementById('olho').style.display = "block"
+    document.getElementById('angry').play()
+}
+function nao() {
+    document.getElementById('nao').style.display = "block"
+    document.getElementById('sad').play()
+}
