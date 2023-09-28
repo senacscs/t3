35e2a7f6c0b91920ff56f50ed5a678cfbd941c63
dudubds26 <132916615+dudubds26@users.noreply.github.com>
2023-09-28 19:49:25 -0300

    att

---
+++ eduardo/deusegipcio/quiz/script.js
@@ -0,0 +1,5 @@
+function respostas() {
+    var resposta = document.getElementById("submit-btn").value
+
+    alert("As repostas corretas são:\n\n1) 14\n\n2) Ísis\n\n3) Inveja\n\n4) set se transformou em um animal e despedaçou osiris e\nfoi preso em sarcofago e jogado no rio Nilo\n\n5) Aproximadamente: Osíris era um deus egípcio associado à morte e à ressurreição. Ele foi assassinado por seu irmão Seth, mas sua esposa, Ísis, o ressuscitou temporariamente. Osíris se tornou o governante do submundo, simbolizando a vida após a morte e a esperança de renascimento espiritual.")
+}
