7a7f3a4425613553eb9f760cde194dab77830ecf
Isaac P. Ramos <132916971+zaazexe@users.noreply.github.com>
2023-09-29 09:52:17 -0300

    att deus egipcio

---
+++ isaac/deusegipcio/quiz/script.js
@@ -0,0 +1,7 @@
+function respostas() {
+    alert("1) Guerra" +
+    "\n\n2) Porco da Terra" +
+    "\n\n3) Hórus" + 
+    "\n\n4) Néftis e Anat" +
+    "\n\n5) Porque ele tinha inveja de Osíris.")
+}
