abaf2beb79786e717867cb2806d4a6ebd57b7a35
dudubds26 <132916615+dudubds26@users.noreply.github.com>
2023-10-09 10:38:14 -0300

    up

--- eduardo/logica-programacao/ex1/script.js
+++
@@ -1,6 +0,0 @@
-var dinheiro = parseFloat(prompt("Digite um valor: "))
-var dolar = (dinheiro / 5.13).toFixed(2)
-var euro = (dinheiro / 5.40).toFixed(2)
-var libras = (dinheiro / 6.24).toFixed(2)
-
-alert("Com R$" + dinheiro + "\nVocê pode comprar " + dolar + " dólares" + "\nVocê pode comprar " + euro + " euros" + "\nVocê pode comprar " + libras + " libras.")
