ad41c34f43b3a6b0adc9f5be953b6a82288b26c5
Gu-oliveir <132953994+Gu-oliveir@users.noreply.github.com>
2023-10-17 09:05:27 -0300

    .

--- gustavoo/jogojs/script.js
+++ gustavoo/jogojs/script.js
@@ -1,9 +1,19 @@
-const currentPlayer = document.querySelector(".currentPlayer");
+// Player X = 0
+// Player O = 1
 
-let selected;
-let player = "X";
+let currentPlayer = 0;
 
-let positions = [
+const circleElement =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle"><circle cx="12" cy="12" r="10"/></svg>';
+
+const crossElement =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>';
+
+const messageElement = document.getElementById("message");
+
+let gameState = {};
+
+const possiblePositions = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9],
@@ -14,52 +24,79 @@ let positions = [
   [3, 5, 7],
 ];
 
-function init() {
-  selected = [];
+function updateMessage(msg) {
+  messageElement.innerHTML = msg;
+}
 
-  currentPlayer.innerHTML = `JOGADOR DA VEZ: ${player}`;
+function getElementByPlayer() {
+  if (currentPlayer === 0) {
+    return crossElement;
+  }
 
-  document.querySelectorAll(".game button").forEach((item) => {
-    item.innerHTML = "";
-    item.addEventListener("click", newMove);
+  if (currentPlayer === 1) {
+    return circleElement;
+  }
+}
+
+function createGame() {
+  // The initial player
+  currentPlayer = Math.floor(Math.random() * 2);
+  updateMessage(`Vez de ${getElementByPlayer()}`);
+}
+
+function validatePlay() {
+  let gameEnded = false;
+  let winner = -1;
+
+  possiblePositions.forEach((possiblePosition) => {
+    if (
+      gameState[possiblePosition[0]] === gameState[possiblePosition[1]] &&
+      gameState[possiblePosition[1]] === gameState[possiblePosition[2]] &&
+      typeof gameState[possiblePosition[1]] === "number"
+    ) {
+      gameEnded = true;
+      winner = gameState[possiblePosition[0]];
+    }
+  });
+
+  if (!gameEnded) return false;
+
+  updateMessage(`${getElementByPlayer(winner)} venceu`);
+
+  setTimeout(() => {
+    gameState = {};
+    createGame();
+  }, 5000);
+
+  return true;
+}
+
+function nextPlayer() {
+  // Update the index for the next player
+  currentPlayer = currentPlayer === 0 ? 1 : 0;
+}
+
+function registerButtons() {
+  const buttons = document.querySelectorAll(".button-position");
+
+  buttons.forEach((button) => {
+    const index = button.getAttribute("data-index");
+    button.addEventListener("click", () => {
+      if (typeof gameState[index] === "undefined") {
+        gameState[index] = currentPlayer;
+
+        button.innerHTML = getElementByPlayer();
+
+        if (validatePlay()) {
+          return;
+        }
+
+        nextPlayer();
+        updateMessage(`Vez de ${getElementByPlayer()}`);
+      }
+    });
   });
 }
 
-init();
-
-function newMove(e) {
-  const index = e.target.getAttribute("data-i");
-  e.target.innerHTML = player;
-  e.target.removeEventListener("click", newMove);
-  selected[index] = player;
-
-  setTimeout(() => {
-    check();
-  }, [100]);
-
-  player = player === "X" ? "O" : "X";
-  currentPlayer.innerHTML = `VEZ DO JOGADOR: ${player}`;
-}
-
-function check() {
-  let playerLastMove = player === "X" ? "O" : "X";
-
-  const items = selected
-    .map((item, i) => [item, i])
-    .filter((item) => item[0] === playerLastMove)
-    .map((item) => item[1]);
-
-  for (pos of positions) {
-    if (pos.every((item) => items.includes(item))) {
-      alert("O JOGADOR '" + playerLastMove + "' GANHOU!");
-      init();
-      return;
-    }
-  }
-
-  if (selected.filter((item) => item).length === 9) {
-    alert("DEU EMPATE!");
-    init();
-    return;
-  }
-}
+createGame();
+registerButtons();
