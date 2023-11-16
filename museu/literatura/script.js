e6f14cdfa45f2b62ec40798a3a3fafc8150aea4c
dudubds26 <132916615+dudubds26@users.noreply.github.com>
2023-11-16 14:18:54 -0300

    up literatura

---
+++ museu/literatura/script.js
@@ -0,0 +1,87 @@
+function machado(){
+    const button = document.getElementById("machado-button")
+    const popup = document.getElementById("machado")
+    const closeButton = document.getElementById("close-machado")
+
+
+        popup.showModal()
+    
+
+    closeButton.onclick = function() {
+        popup.close()
+    }
+
+}
+
+function jesus_funcionapfv() {
+    const button = document.getElementById("jesus-button")
+    const popup = document.getElementById("jesus")
+    const closeButton = document.getElementById("close-jesus")
+
+
+        popup.showModal()
+
+    closeButton.onclick = function() {
+        popup.close()
+    }
+
+}
+
+function conceicao() {
+    const button = document.getElementById("conceicao-button")
+    const popup = document.getElementById("conceicao")
+    const closeButton = document.getElementById("close-conceicao")
+
+
+        popup.showModal()
+    
+
+    closeButton.onclick = function() {
+        popup.close()
+    }
+}
+
+function martin() {
+    const button = document.getElementById("martin-button")
+    const popup = document.getElementById("martin")
+    const closeButton = document.getElementById("close-martin")
+
+
+        popup.showModal()
+    
+
+    closeButton.onclick = function() {
+        popup.close()
+    }
+}
+
+function saci() {
+    const button = document.getElementById("saci-button")
+    const popup = document.getElementById("saci")
+    const closeButton = document.getElementById("close-saci")
+
+
+        popup.showModal()
+    
+
+    closeButton.onclick = function() {
+        popup.close()
+    }
+}
+
+function negrinho() {
+    const button = document.getElementById("negrinho-button")
+    const popup = document.getElementById("negrinho")
+    const closeButton = document.getElementById("close-negrinho")
+
+
+        popup.showModal()
+    
+
+    closeButton.onclick = function() {
+        popup.close()
+    }
+}
+
+
+
