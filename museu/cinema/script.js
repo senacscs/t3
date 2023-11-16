bb0a9d7f3862e1c0ef4438d97ef8df6ceb3532fd
isadorakroth <132912701+Isakroth@users.noreply.github.com>
2023-11-16 10:46:06 -0300

    cinema

--- museu/cinema/script.js
+++ museu/cinema/script.js
@@ -1,121 +1,86 @@
-$(".item--carousel").click(function () {
-  // Item already active
-  var elAlreadyActive = $(".item--carousel.active");
-  // item already active position
-  var elAlreadyActivePosition = $(".item--carousel.active").index();
+$(document).ready(function() {
+  // Ativação do item do carrossel ao ser clicado
+  $(".item--carousel").click(function() {
+    var elAlreadyActivePosition = $(".item--carousel.active").index();
+    $(".item--carousel").removeClass("active");
+    var elNowActive = $(this).addClass("active");
+    $(this).removeClass("showme");
 
-  $(".item--carousel").removeClass("active");
-  var elNowActive = $(this).addClass("active");
-  $(this).removeClass("showme");
-  // how many elements carousel has
-  var elLenght = $(".item--carousel").length;
-  console.log("ElementLength: " + elLenght);
-  // active element position now
-  var elPosition = $(this).index();
-  console.log("activePosition: " + elPosition);
-  // element before active item
-  var elBeforeThis = $(this).prev().index();
-  console.log("positionBefore: " + elBeforeThis);
-  // element after active item
-  var elAfterThis = $(this).next().index();
-  console.log("positionAfter: " + elAfterThis);
+    var elPosition = $(this).index();
 
-  if (elBeforeThis >= 0 && elAfterThis != -1) {
-    console.log("Lets Slide");
+    // Verifica se há elementos antes e depois do item ativo
+    var elBeforeThis = $(this).prev().index();
+    var elAfterThis = $(this).next().index();
 
-    if (elPosition > elAlreadyActivePosition) {
-      $(this).prev().prev().addClass("hideme");
-      $(this).prev().prev().addClass("redux");
-      console.log("Its Superior");
-    } else if (elPosition < elAlreadyActivePosition) {
-      $(this).prev().removeClass("hideme");
-      $(this).prev().removeClass("redux");
-      $(this).prev().addClass("showme");
-
-      console.log("Its Not Superior");
-    }
-  } else {
-    console.log("We Cant Slide");
-  }
-});
-
-//actualizar a posicao da class active no carrosel de testemuhos
-if ($(window).width() < 768) {
-  $(".inner--carousel .item--carousel").removeClass("active");
-  $(".inner--carousel .item--carousel").first().addClass("active");
-} else {
-  $(".inner--carousel .item--carousel").removeClass("active");
-  $(".inner--carousel .item--carousel").first().next().addClass("active");
-}
-
-$(window).resize(function () {
-  $("window")
-    .$("body")
-    .prepend("<div>" + $(window).width() + "</div>");
-
-  //actualizar a posicao da class active no carrosel de testemuhos
-  if ($(window).width() < 768) {
-    $(".inner--carousel .item--carousel").removeClass("active");
-    $(".inner--carousel .item--carousel").first().addClass("active");
-  } else {
-    $(".inner--carousel .item--carousel").removeClass("active");
-    $(".inner--carousel .item--carousel").first().next().addClass("active");
-  }
-});
-
-$(".indicators--carousel span").click(function () {
-  $(".indicators--carousel span").removeClass("active");
-  $(this).addClass("active");
-  var pos = $(this).index();
-  $(".inner--carousel").find(".item--carousel").removeClass("active");
-  $(".inner--carousel").find(".item--carousel").css("order", "10");
-  var activeItem = $(".item--carousel").eq(pos);
-  $(activeItem).addClass("active");
-  $(activeItem).css("order", 1);
-});
-
-//mobile swipe
-$(".person-carousel .inner--carousel").on("touchstart", function (event) {
-  var xClick = event.originalEvent.touches[0].pageX;
-  $(this).one("touchmove", function (event) {
-    var xMove = event.originalEvent.touches[0].pageX;
-    if (Math.floor(xClick - xMove) > 5) {
-      console.log("left");
-      funHoldActive();
-      function funHoldActive() {
-        var active = $(".item--carousel.active");
-        if (!active.is(".item--carousel:first")) {
-          $(".item--carousel").removeClass("active");
-          $(".item--carousel").css("order", "10");
-          $(active).prev().addClass("active");
-          $(active).prev().css("order", 1);
-          $(".indicators--carousel span.active")
-            .removeClass("active")
-            .prev()
-            .addClass("active");
-        }
-      }
-    } else if (Math.floor(xClick - xMove) < -5) {
-      console.log("right");
-      okokXD();
-      function okokXD() {
-        var active = $(".item--carousel.active");
-        if (!active.is(".item--carousel:last")) {
-          $(".item--carousel").css("order", "10");
-          $(".item--carousel.active")
-            .removeClass("active")
-            .next()
-            .css("order", "1")
-            .addClass("active");
-          $(".indicators--carousel span.active")
-            .removeClass("active")
-            .next()
-            .addClass("active");
-        }
+    if (elBeforeThis >= 0 && elAfterThis !== -1) {
+      if (elPosition > elAlreadyActivePosition) {
+        $(this).prev().prev().addClass("hideme redux");
+      } else if (elPosition < elAlreadyActivePosition) {
+        $(this).prev().removeClass("hideme redux");
+        $(this).prev().addClass("showme");
       }
     }
   });
-  $(".person-carousel .inner--carousel").on("touchend", function () {
-    $(this).off("touchmove");
+
+  // Atualiza a posição do item ativo no carrossel de acordo com a largura da janela
+  function updateActiveItemPosition() {
+    if ($(window).width() < 768) {
+      $(".inner--carousel .item--carousel").removeClass("active");
+      $(".inner--carousel .item--carousel").first().addClass("active");
+    } else {
+      $(".inner--carousel .item--carousel").removeClass("active");
+      $(".inner--carousel .item--carousel").first().next().addClass("active");
+    }
+  }
+
+  // Chamada da função ao carregar a página e redimensionar a janela
+  updateActiveItemPosition();
+  $(window).on('resize', function() {
+    updateActiveItemPosition();
   });
+
+  // Controle dos indicadores do carrossel
+  $(".indicators--carousel span").click(function() {
+    $(".indicators--carousel span").removeClass("active");
+    $(this).addClass("active");
+    var pos = $(this).index();
+    $(".inner--carousel .item--carousel").removeClass("active").css("order", "10");
+    var activeItem = $(".item--carousel").eq(pos);
+    $(activeItem).addClass("active").css("order", 1);
+  });
+
+  // Swipe em dispositivos móveis
+  $(".person-carousel .inner--carousel").on("touchstart", function(event) {
+    var xClick = event.originalEvent.touches[0].pageX;
+    $(this).one("touchmove", function(event) {
+      var xMove = event.originalEvent.touches[0].pageX;
+      var distance = xClick - xMove;
+
+      if (distance > 5) {
+        // Swipe para a esquerda
+        moveCarousel('left');
+      } else if (distance < -5) {
+        // Swipe para a direita
+        moveCarousel('right');
+      }
+    });
+    $(".person-carousel .inner--carousel").on("touchend", function() {
+      $(this).off("touchmove");
+    });
+  });
+
+  // Função para mover o carrossel
+  function moveCarousel(direction) {
+    var active = $(".item--carousel.active");
+
+    if (direction === 'left' && !active.is(".item--carousel:first")) {
+      active.removeClass("active").css("order", "10");
+      active.prev().addClass("active").css("order", "1");
+      $(".indicators--carousel span.active").removeClass("active").prev().addClass("active");
+    } else if (direction === 'right' && !active.is(".item--carousel:last")) {
+      active.removeClass("active").css("order", "10");
+      active.next().addClass("active").css("order", "1");
+      $(".indicators--carousel span.active").removeClass("active").next().addClass("active");
+    }
+  }
 });
