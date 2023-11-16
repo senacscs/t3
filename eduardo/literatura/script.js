42ccf49a5902972aef132bd11bdc4176296501e7
dudubds26 <132916615+dudubds26@users.noreply.github.com>
2023-11-16 12:18:36 -0300

    att

---
+++ eduardo/literatura/script.js
@@ -0,0 +1,88 @@
+function machado(){
+    const button = document.getElementById("machado-button")
+    const popup = document.getElementById("machado")
+    const closeButton = document.getElementById("close-machado")
+
+    button.onclick = function() {
+        popup.showModal()
+    }
+
+    closeButton.onclick = function() {
+        popup.close()
+    }
+
+}
+
+function jesus_funcionapfv() {
+    const button = document.getElementById("jesus-button")
+    const popup = document.getElementById("jesus")
+    const closeButton = document.getElementById("close-jesus")
+
+    button.onclick = function() {
+        popup.showModal()
+    }
+
+    closeButton.onclick = function() {
+        popup.close()
+    }
+
+}
+
+function conceicao() {
+    const button = document.getElementById("conceicao-button")
+    const popup = document.getElementById("conceicao")
+    const closeButton = document.getElementById("close-conceicao")
+
+    button.onclick = function() {
+        popup.showModal()
+    }
+
+    closeButton.onclick = function() {
+        popup.close()
+    }
+}
+
+function martin() {
+    const button = document.getElementById("martin-button")
+    const popup = document.getElementById("martin")
+    const closeButton = document.getElementById("close-martin")
+
+    button.onclick = function() {
+        popup.showModal()
+    }
+
+    closeButton.onclick = function() {
+        popup.close()
+    }
+}
+
+function saci() {
+    const button = document.getElementById("saci-button")
+    const popup = document.getElementById("saci")
+    const closeButton = document.getElementById("close-saci")
+
+    button.onclick = function() {
+        popup.showModal()
+    }
+
+    closeButton.onclick = function() {
+        popup.close()
+    }
+}
+
+function jesus() {
+    const button = document.getElementById("negrinho-button")
+    const popup = document.getElementById("negrinho")
+    const closeButton = document.getElementById("close-negrinho")
+
+    button.onclick = function() {
+        popup.showModal()
+    }
+
+    closeButton.onclick = function() {
+        popup.close()
+    }
+}
+
+
+
