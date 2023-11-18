27c6bd4af4d4e7c0abb34e19a90d7f5c8cb4d004
AndreNicolay <132912126+AndreNicolay@users.noreply.github.com>
2023-11-18 15:18:30 -0300

    .

---
+++ andre/projeto/script.js
@@ -0,0 +1,48 @@
+const sections = {
+    introducao: '## Introdução\nO Nosso projeto foi pensado e elaborado, para que o descarte de medicamentos vencidos seja feito de forma correta, procurando formas e maneiras de informar as pessoas, de como realizar esse descarte da maneira certa e segura.\n## Tema e área do conhecimento\n**Tema:** Reciclagem de blisters e outros tipos de resíduos médicos.\n\n**Área:** Saúde e Bem-estar.',
+    problema: '## Problema\nComo aumentar a reciclagem dos medicamentos vencidos?',
+    justificativa: '## Justificativa\nPor meio de ensinos e até mesmo um aplicativo pode analisar e começar a mostrar na justificativa que os resíduos farmacêuticos não são reciclados ou retirados por meios certos ou com sucesso. As capsulas de remédios tóxicas ao corpo humano, por isso deve se reciclar direito pois os malefícios da reciclagem são altos como levar à formação de ilhas de lixos, plástico e papel descartados nos rios causam enchentes e alagamentos nas grandes cidades, disseminam doenças, causam a morte das populações e prejudicam a fauna de diversas áreas. Além deste problema, as pessoas dificilmente conseguiriam conviver ao lado de montes de lixo e até mesmo resíduos altamente tóxico e se convive é porque não possuem rendimento para sair ou ajuda pra resolver este problema. Procurando resolver este problema se vê que as metas do projeto seriam tentar ao menos amenizar uma parte da reciclagem com estes produtos onde as vezes as pessoas não sabem como descartar.',
+    objetivos: '## Objetivos\nSendo o remédio um meio de droga que as pessoas utilizam para melhorar sua saúde e bem-estar, trazendo benefícios para cura de problemas internos na saúde. Mas todos sabem que estes resíduos vêm com pacotes e cartelas metálicas ou de plásticos, onde as pessoas não possuem devido ensinamento para reciclagem destes produtos. Como resolução, apresentamos mais incentivo as pessoas de aprender ou até mesmo pesquisar como se livrar, principalmente as cartelas metálicas e as caixas, indo em sites, mostrando seus danos a natureza e também a vida humana por tóxicos espalhados. Com o foco em atingir todos os públicos, de adolescentes até mesmo adultos ou crianças, mas principalmente idosos onde possuem maior porcentagem de uso de remédios por sua saúde ou seus anticorpos estarem enfraquecendo, com a intenção sempre da reciclagem ser aumentada. Dados apresentados pelas pessoas que compraram e usufruíram descreveram ao ir jogar fora pelo menos uma vez na vida foi relatado por 55% das pessoas só jogavam estes produtos no lixo ou descartavam indevidamente. Um dos meios mais utilizados pela sociedade com a intenção, mas com incentivo de aumentar a reciclagem é ongs ou aulas e ensinamentos sobre estes meios para a sociedade.\n\n**Objetivo Geral:**\nIncentivar o aumento de reciclagem dos resíduos medicais que não sabem onde descartar.\n\n**Objetivos específicos:**\n-  Como reciclar estes produtos. \n- Como controlar/descartar estes resíduos. \n- Meios da reciclagem destes produtos.',
+    referencial: '## Referêncial teórico\nBuscando conscientizar as pessoas sobre o descarte de medicamentos vencidos descartados nos lixos errados, é um tópico a ser conversado, discutido e de extrema necessidade dentro da sociedade e comunidades. Geralmente quando os medicamentos que não têm mais utilidade ou valor para nós seres humanos, descartamos os medicamentos dentro do lixo de casa ou pela privada de nossos banheiros. Com isso podemos causar grandes problemas aos mares e solos da terra, os medicamentos descartados de maneira errada soltam substâncias químicas e nocivas ao solo. Assim afetando também os animais terrestres ou qualquer pessoa que entre em contato com aquele medicamento no chão. Outros exemplos de descarte errado dos medicamentos vencidos, quando se é descartado nas linhas de esgoto isso consequentemente afetará a água, assim preocupando a todos. Então, como descartar os medicamentos vencidos de forma correta? Uma das melhores alternativas seria através dos Pontos de Coletas ou Farmácias, nestes lugares o descarte será realizado de forma segura e correta. “Por menor que pareça, a ação de descartar o medicamento de forma incorreta acaba causando malefícios não visíveis, como a contaminação do solo, do lençol freático e dos rios. Há um grande risco para população.” Afirmou Walesca Lacerda, engenheira ambiental. Até 2024 o Brasil pode atingir a marca de sexto país que mais consume medicamentos no mundo, a projeção é de que as vendas ultrapassem os US$ 38 bilhões, isso parece ser um dado positivo pela saúde estar melhorando, mas quando o assunto é o descarte das cartelas dos remédios, o Brasil está bem mal. Karolina Marques (professora do curso de Farmácia da Uninter), relembra que o maior problema do descarte de medicamentos não está relacionado aos hospitais ou indústrias do ramo, mas ao uso doméstico. “A maioria não dispensa os medicamentos de maneira incorreta porque querem contaminar, é porque a maioria não sabe o que fazer com aquelas sobras”, diz. Quais foram os medicamentos encontrados nos rios de alguns países. Veja abaixo: Alemanha: anti-inflamatório, analgésico, antipirético, anti-hipertensivos. Reino Unido: anti-hipertensivos. Itália: furosemida, atenolol, antibióticos, ibuprofeno, entre outros. Além dessa eliminação de forma natural, temos ainda a criação das “farmacinhas” dentro de casa, que foram impulsionadas pelo fácil acesso à medicamentos com baixo custo ou gratuitos, sem falar no elevado número de farmácias – cerca de uma para cada 3.300 habitantes, segundo dados do Conselho Federal de Farmácia. Ou seja, suponhamos que temos um paciente que necessite de um antibiótico durante 10 dias, mas só é possível comprar ou pegar no posto de saúde o medicamento com a caixa de 30 comprimidos. O que fazer com esse excedente? A maioria das pessoas armazena para quando precisarem novamente. Guardar um remédio em casa não é uma atitude correta. E muitas pessoas podem até querer utilizá-los sem necessidade para não perder o medicamento.',
+    metodologia: '## Metodologia\nO principal problema, conforme já descrito, é a falta de conhecimento em geral, tornando-se cada vez mais evidente que as pessoas só os utilizam e jogam em qualquer local designado como lata de lixo a seu ver após o uso. A Justificativa destes ensinamentos busca através de uma análise como criar atos ou materiais para que este projeto realmente aconteça e seja válido. Pessoas sem consciência disso acabam apenas jogando no lixo e nem mesmo indo a instituições ou ONGs para reciclar as embalagens e até mesmo os remédios. Um local adequado para nem “descartar”, mas sim ajudar os recicladores, é nas farmácias. As farmácias podem incentivar isso oferecendo promoções de medicamentos ou algo parecido.',
+    resultados: '## Resultados e discussões\nAnalisando os resultados, vemos que mesmo estando em 2023 as pessoas não sabem reciclar as cartelas(blister) de remédios utilizadas até por elas mesmo ou vendido e que precisa ser desenvolvido para médicos ou farmacêuticos o ensinamento de como descartar estes produtos feitos de PVC, PVDC e alumínio, um material que afeta bastante nosso meio ambiente, liberando gases tóxicos afetando água, solo, animais, ar e até mesmo nossos alimentos que comemos no dia a dia, ocasionando diretamente a doenças crônicas e perigosas. Descartar esses itens com exatidão, trará bastante um meio de melhoria ambiental ao nosso planeta mesmo começando aos poucos a ir em farmácias ou pontos de recolhimento feito por mercados e postos de saúde que iram reciclar corretamente.',
+    referencias: '## Referências\n- SÃO PAULO. (Taboão da Serra) RCR Ambiental. In: RECICLAGEM DE BLISTER. 20 jul. 2023. Disponível em: <https://rcrambiental.com.br/reciclagem/reciclagem-de-blister//>\n\n- SÃO PAULO. (Estado) Recicla Sampa. 20 jul. 2023. COMO DESCARTAR CARTELAS DE REMÉDIOS VAZIAS EM SP. Disponível em: <https://www.reciclasampa.com.br/artigo/como-descartar-cartelas-de-remedios-vazias-em-sp>',
+}
+
+function renderContent(content) {
+    const contentElement = document.getElementById('content')
+
+    // Starts the fade animation
+    contentElement.style.opacity = 0
+
+    // Applies the content and returns the opacity after 200ms
+    setTimeout(() => {
+        contentElement.innerHTML = marked.parse(content)
+        contentElement.style.opacity = 1
+    }, 200);
+}
+
+function renderContentBySectionName(sectionName) {
+    const section = sections[sectionName]
+    renderContent(section)
+}
+
+function handleNavbar() {
+    const buttons = document.querySelectorAll('nav button')
+
+    // Handles every nav button when clicking
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            renderContentBySectionName(
+                button.getAttribute('data-section')
+            )
+
+            setTimeout(() => {
+                window.location.href = '#content'
+            }, 200);
+        })
+    })
+}
+
+renderContent(sections.introducao)
+handleNavbar()
