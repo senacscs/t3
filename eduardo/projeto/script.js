6f544532baefa9d6bd477c2aa02243e220fb47f5
dudubds26 <132916615+dudubds26@users.noreply.github.com>
2023-11-21 12:21:33 -0300

    att

--- eduardo/projeto/script.js
+++ eduardo/projeto/script.js
@@ -10,3 +10,11 @@ function formPopUp(){
     }
 
 }
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
