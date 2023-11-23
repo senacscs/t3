7b729821f196e715e84f27c63960a0010d8b2d13
Gu-oliveir <132953994+Gu-oliveir@users.noreply.github.com>
2023-11-23 08:22:51 -0300

    .

--- gustavoo/projeto/script.js
+++ gustavoo/projeto/script.js
@@ -13,3 +13,7 @@ const html = document.querySelector('html')
 function modoNoturno() {
     html.classList.toggle('dark-mode')
 }
+
+document.getElementById('icon').addEventListener('click', function () {
+    alert('Mini Mercado e Açougue Oliveira – Rio Pardinho - RS \n\n\n- Qual sistema é usado para controle de produtos? \n\nO sistema usado é Compumax. \n\n\n- Como é feito o controle da validade dos produtos? \n\nÉ feito manualmente. \n\n\n- O mercado tem o controle total sobre o vencimento de todos os produtos dentro do estoque? \n\nO mercado não tem controle total dos produtos vencidos. \n\n\n- Já tiveram perda de produtos que ficaram vencidos nas prateleiras? Essa perda afetou o lucro do mercado? Acontece com muita frequência? \n\nSim, a perda não foi muito significativa, aconteceu uma vez. \n\n\n- Já foram prejudicados pela fiscalização por conta de produtos vencidos nas  \n\nprateleiras? Ou denúncias? \n\nSim. \n\n\n- Se o projeto for aplicado, ele seria relevante? O que não pode faltar no projeto? Algo essencial. \n\nSeria relevante.')
+});
