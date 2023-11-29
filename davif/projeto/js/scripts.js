1285ab95a62e53f29d2ad9d54ea6b2d9b5fc1a82
davifroemming2 <133661291+davifroemming2@users.noreply.github.com>
2023-11-29 14:36:38 -0300

    Add files via upload

---
+++ davif/projeto/js/scripts.js
@@ -0,0 +1,10 @@
+function olaMundo() {
+  document.getElementById("mostrar").innerHTML = "Bem vindo ao projeto Davi!";
+  console.log(`Bem vindo ao projeto Davi!`);
+  alert(`Bem vindo ao projeto Davi!`);
+}
+
+function numeroDigitado() {
+  document.getElementById("mostrar").innerHTML =
+    "O número digitado foi " + numero.value + " e seu triplo é" + numero. value ;
+}
