239e2cb4413fed5306a428b50a4bdfda85672764
PedroHST120 <133657229+PedroHST120@users.noreply.github.com>
2023-12-04 11:24:47 -0300

    todo

---
+++ pedro/todo/main.js
@@ -0,0 +1,8 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    console.log()
