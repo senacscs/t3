3ba538b2a37f6994387b163199ef5b3668015572
FMaieron <132914947+FMaieron@users.noreply.github.com>
2023-12-04 11:23:23 -0300

    .

---
+++ francesco/todolist/main.js
@@ -0,0 +1,22 @@
+console.log('ta tela')
+let form = document.getElementById('form');
+let input =  document.getElementById('input');
+let msg = document.getElementById('msg');
+let posts =  document.getElementById('posts');
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("button clicked");
+  
+    formValidation();
+  });
+  
+  let formValidation = () => {};
+  
+    if (input.value === "") {
+      msg.innerHTML = "Post cannot be blank";
+      console.log("failure");
+    } else {
+      console.log("successs");
+      msg.innerHTML = "";
+    };
