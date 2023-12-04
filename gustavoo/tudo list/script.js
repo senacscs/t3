ce67881c4a39c2cd4636ad5e497a671232d5fe71
Gustavo Oliveira <132953994+Gu-oliveir@users.noreply.github.com>
2023-12-04 11:17:51 -0300

    .

---
+++ gustavoo/tudo list/script.js
@@ -0,0 +1,21 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("button clicked");
+  
+    formValidation();
+  });
+  
+  let formValidation = () => {
+    if (input.value === "") {
+      msg.innerHTML = "Post cannot be blank";
+      console.log("failure");
+    } else {
+      console.log("successs");
+      msg.innerHTML = "";
+    }
+  };
