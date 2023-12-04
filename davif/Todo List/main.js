70e0e0148d25a9cf5e1442f2d2c5eca5c2d93265
davifroemming2 <133661291+davifroemming2@users.noreply.github.com>
2023-12-04 11:34:39 -0300

    Add files via upload

---
+++ davif/Todo List/main.js
@@ -0,0 +1,44 @@
+alert("isso tá funcionando");
+
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("button clicked");
+  
+    formValidation();
+  });
+  
+
+  let formValidation = () => {
+    if (input.value === "") {
+      msg.innerHTML = "Post cannot be blank";
+      console.log("failure");
+    } else {
+      console.log("successs");
+      msg.innerHTML = "";
+    }
+  };
+
+  let data = {};
+
+let acceptData = () => {
+    data["text"] = input.value;
+    console.log(data);
+  };
+
+  let createPost = () => {
+    posts.innerHTML += `
+    <div>
+      <p>${data.text}</p>
+      <span class="options">
+        <i onClick="editPost(this)" class="fas fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+      </span>
+    </div>
+    `;
+    input.value = "";
+  };
