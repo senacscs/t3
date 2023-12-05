6e6d41926d8bc82329b25be0fa211f2258285779
diaszxqz <133666183+diaszxqz@users.noreply.github.com>
2023-12-05 09:27:43 -0300

    removi para colocar o novo

--- lucas/todolist/js/main.js
+++ lucas/todolist/js/main.js
@@ -7,16 +7,23 @@ let posts = document.getElementById("posts");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("button clicked");
- 
     formValidation();
 });
  
 let formValidation = () => {
-    if (input.value ==="") {
+    if (input.value === "") {
         msg.innerHTML = "Post cannot be blank";
         console.log("failure");
     } else {
         console.log("successs");
         msg.innerHTML = "";
+        acceptData();
     }
+};
+
+let data = {};
+
+let acceptData = () => {
+    data["text"] = input.value;
+    console.log(data);
 };
