e4aed3d640178b997d20b2ca3c52fcb3adc3b484
LudovicoMohr <132920559+LudovicoMohr@users.noreply.github.com>
2023-12-06 12:25:16 -0300

    up

--- francesco/todolist/main.js
+++ francesco/todolist/main.js
@@ -1,8 +1,7 @@
-console.log('ta tela')
-let form = document.getElementById('form');
-let input =  document.getElementById('input');
-let msg = document.getElementById('msg');
-let posts =  document.getElementById('posts');
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -11,12 +10,46 @@ form.addEventListener("submit", (e) => {
     formValidation();
   });
   
-  let formValidation = () => {};
-  
+  let formValidation = () => {
     if (input.value === "") {
-      msg.innerHTML = "Post cannot be blank";
-      console.log("failure");
-    } else {
-      console.log("successs");
-      msg.innerHTML = "";
-    };
+        msg.innerHTML = "Post cannot be blank";
+        console.log("failure");
+      } else {
+        console.log("successs");
+        msg.innerHTML = "";
+        acceptData();
+
+      }
+  };
+
+let data = {};
+
+let acceptData = () => {
+    data["text"] = input.value;
+    console.log(data);
+    
+  createPost();
+};
+
+let createPost = () => {
+    posts.innerHTML += ``;
+    posts.innerHTML += `
+    <div>
+      <p>${data.text}</p>
+      <span class="options">
+        <i onClick="editPost(this)" class="fas fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+      </span>
+    </div>
+    `;
+    input.value = "";
+  };
+ 
+  let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
+  };
+
+  let editPost = (e) => {
+    input.value = e.parentElement.previousElementSibling.innerHTML;
+    e.parentElement.parentElement.remove();
+  };
