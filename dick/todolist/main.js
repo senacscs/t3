e6c4ae206410bf20700ce525b34a2ed1c5e5fef9
gabolopes <132939952+gabolopes@users.noreply.github.com>
2023-12-06 11:55:52 -0300

    deleta errado

--- dick/todolist/main.js
+++ dick/todolist/main.js
@@ -18,5 +18,29 @@ form.addEventListener("submit", (e) => {
     } else {
       console.log("successs");
       msg.innerHTML = "";
+      acceptData();
     }
   };
+
+  let data = {};
+
+let acceptData = () => {
+  data["text"] = input.value;
+  console.log(data);
+  createPost();
+};
+
+
+
+let createPost = () => {
+  posts.innerHTML += `
+  <div>
+    <p>${data.text}</p>
+    <span class="options">
+      <i onClick="editPost(this)" class="fas fa-edit"></i>
+      <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+    </span>
+  </div>
+  `;
+  input.value = "";
+};
