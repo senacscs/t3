b1a220a19f836c3ee9ca9a56b8172b0ecd6a3923
EduLunardi0130 <132913101+EduLunardi0130@users.noreply.github.com>
2023-12-06 10:54:47 -0300

    novo index

--- eduarda/script.js
+++ eduarda/script.js
@@ -1,8 +1,30 @@
-const greetingMessage = () => { 
-	let h = new Date().getHours();  
-	if (h <= 5) return 'Boa madrugada';
-	if (h < 12) return 'Bom dia';
-	if (h < 18) return 'Boa tarde';
- 	return 'Boa noite'; 
-}  
-document.getElementById("mensagem").innerHTML =(greetingMessage());
+var dropdown = document.querySelectorAll(".dropdown");
+var dropdownArray = Array.prototype.slice.call(dropdown, 0);
+dropdownArray.forEach(function (el) {
+  var button = el.querySelector('a[data-toggle="dropdown"]'),
+    menu = el.querySelector(".dropdown-menu"),
+    arrow = button.querySelector("i.icon-arrow");
+
+  button.onclick = function (event) {
+    if (!menu.hasClass("show")) {
+      menu.classList.add("show");
+      menu.classList.remove("hide");
+      arrow.classList.add("open");
+      arrow.classList.remove("close");
+      event.preventDefault();
+    } else {
+      menu.classList.remove("show");
+      menu.classList.add("hide");
+      arrow.classList.remove("open");
+      arrow.classList.add("close");
+      event.preventDefault();
+    }
+  };
+});
+
+Element.prototype.hasClass = function (className) {
+  return (
+    this.className &&
+    new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className)
+  );
+};
