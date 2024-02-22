be822595ad2e6bfc6ec5710d86b25ef5468658de
LucianoFloresH <132918731+LucianoFloresH@users.noreply.github.com>
2024-02-22 11:18:31 -0300

    up

--- luciano/todolist/main.js
+++ luciano/todolist/main.js
@@ -1,64 +1,94 @@
 let form = document.getElementById("form");
-let input = document.getElementById("input");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
 let msg = document.getElementById("msg");
-let posts = document.getElementById("posts");
- 
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
+
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log("button clicked");
- 
-    formValidation();
-  });
- 
-  let formValidation = () => {};
- 
-  let formValidation = () => {
-    if (input.value === "") {
-      msg.innerHTML = "Post cannot be blank";
-      console.log("failure");
-    } else {
-      console.log("successs");
-      msg.innerHTML = "";
-    }
-  };
- 
-  let data = {};
- 
-let acceptData = () => {};
- 
+  e.preventDefault();
+  formValidation();
+});
+
+let formValidation = () => {
+  if (textInput.value === "") {
+    console.log("failure");
+    msg.innerHTML = "Task cannot be blank";
+  } else {
+    console.log("success");
+    msg.innerHTML = "";
+    acceptData();
+    add.setAttribute("data-bs-dismiss", "modal");
+    add.click();
+
+    (() => {
+      add.setAttribute("data-bs-dismiss", "");
+    })();
+  }
+};
+
+let data = [{}];
+
 let acceptData = () => {
-    data["text"] = input.value;
-    console.log(data);
-  };
- 
-  let formValidation = () => {
-    if (input.value === "") {
-      // Other codes are here
-    } else {
-      // Other codes are here
-      acceptData();
-    }
-  };
- 
-  let createPost = () => {
-    posts.innerHTML += ``;
-  };
- 
-  let createPost = () => {
-    posts.innerHTML += `
-    <div>
-      <p>${data.text}</p>
-      <span class="options">
-        <i onClick="editPost(this)" class="fas fa-edit"></i>
-        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
-      </span>
-    </div>
-    `;
-    input.value = "";
-  };
- 
-  let acceptData = () => {
-    // Other codes are here
- 
-    createPost();
-  };
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    description: textarea.value,
+  });
+
+  localStorage.setItem("data", JSON.stringify(data));
+
+  console.log(data);
+  createTasks();
+};
+
+let createTasks = () => {
+  tasks.innerHTML = "";
+  data.map((x, y) => {
+    return (tasks.innerHTML += `
+    <div id=${y}>
+          <span class="fw-bold">${x.text}</span>
+          <span class="small text-secondary">${x.date}</span>
+          <p>${x.description}</p>
+  
+          <span class="options">
+            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+            <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+          </span>
+        </div>
+    `);
+  });
+
+  resetForm();
+};
+
+let deleteTask = (e) => {
+  e.parentElement.parentElement.remove();
+  data.splice(e.parentElement.parentElement.id, 1);
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log(data);
+  
+};
+
+let editTask = (e) => {
+  let selectedTask = e.parentElement.parentElement;
+
+  textInput.value = selectedTask.children[0].innerHTML;
+  dateInput.value = selectedTask.children[1].innerHTML;
+  textarea.value = selectedTask.children[2].innerHTML;
+
+  deleteTask(e);
+};
+
+let resetForm = () => {
+  textInput.value = "";
+  dateInput.value = "";
+  textarea.value = "";
+};
+
+(() => {
+  data = JSON.parse(localStorage.getItem("data")) || []
+  console.log(data);
+  createTasks();
+})();
