c4d6aa25ee401e2f36049d7643c0ebe338ad5448
soso <132917029+sophiascholante@users.noreply.github.com>
2024-02-27 10:50:07 -0300

    excluir

--- sophia/primeiro/linkemusic/home/script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-        const navMenu = document.querySelector(".nav-menu");
-
-        hamburger.addEventListener("click", () => {
-            hamburger.classList.toggle('active');
-            navMenu.classList.toggle('active');
-        })
