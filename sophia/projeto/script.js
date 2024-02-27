b1b949c5c5e3e9ed812a1bd0c39d47b3d36625c4
soso <132917029+sophiascholante@users.noreply.github.com>
2024-02-27 10:44:53 -0300

    att

--- sophia/projeto/script.js
+++
@@ -1,6 +0,0 @@
-var angle = 0;
-function galleryspin(sign) { 
-spinner = document.querySelector("#spinner");
-if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
-spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
-}
