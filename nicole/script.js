9bc2d3037eae47c7239b290fd02ffd5f60aa6cdd
nicolewebers <132921471+nicolewebers@users.noreply.github.com>
2024-06-17 11:13:24 -0300

    up

--- nicole/script.js
+++ nicole/script.js
@@ -1,20 +1,19 @@
 $(document).ready(function() {
-    var header = $("header");
-    var range = 200;
-  
-    $(window).on("scroll", function() {
+  var header = $("header");
+  var range = 200;
+
+  $(window).on("scroll", function() {
       var scrollTop = $(this).scrollTop(),
-        height = header.outerHeight(),
-        offset = height / 2,
-        calc = 1 - (scrollTop - offset + range) / range;
-  
+          height = header.outerHeight(),
+          offset = height / 2,
+          calc = 1 - (scrollTop - offset + range) / range;
+
       header.css({ opacity: calc });
-  
+
       if (calc > 1) {
-        header.css({ opacity: 1 });
+          header.css({ opacity: 1 });
       } else if (calc < 0) {
-        header.css({ opacity: 0 });
+          header.css({ opacity: 0 });
       }
-    });
   });
-  
+});
