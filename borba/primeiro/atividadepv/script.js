dfc929f4aa20d59aefe165e71423ecca925a6463
Davi-Borba <132914458+Davi-Borba@users.noreply.github.com>
2024-06-17 11:01:39 -0300

    vasco3

---
+++ borba/primeiro/atividadepv/script.js
@@ -0,0 +1,10 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
