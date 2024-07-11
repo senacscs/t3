5c4d6ff7c45f0c90d9c81a9d7ad4c4a818317cd2
isadorakroth <132912701+Isakroth@users.noreply.github.com>
2024-07-11 11:55:41 -0300

    mvp

---
+++ isadora/mvp/doação/doacao.js
@@ -0,0 +1,19 @@
+$(".denomination-other input").on('keypress', function (event) {
+  // allow only int values
+  // TODO: remove leading 0
+  var regex = new RegExp("^[0-9]+$");
+  var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+  if (!regex.test(key)) {
+    event.preventDefault();
+    return false;
+  }
+  
+  // Update hidden radio button and button text
+  var matchingRadio = $(`input[name="amount"][value=${event.key}]`);
+  matchingRadio.prop('checked', true);
+  $("button").text('Donate $' + $(this).val() + key );
+  
+  // Remove selection from pre-defined amounts (optional)
+  $(".denomination").removeClass("selected").prop('checked', false);
+  $(this).addClass("selected");
+});
