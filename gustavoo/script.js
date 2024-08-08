f5861b04056e4f2662b27900366ffe579d0d120e
Gustavo Oliveira <132953994+Gu-oliveir@users.noreply.github.com>
2024-08-08 11:23:42 -0300

    .

---
+++ gustavoo/script.js
@@ -0,0 +1,12 @@
+function myFunction() { 
+    document.getElementById("demo").innerHTML = "Todos trabalhos feito em 2023 por mim!";
+}
+function myFunction2() { 
+    document.getElementById("demo2").innerHTML = "Foi feito um site de explicação de back-end, com o mesmo intuito do primeiro site de 2023 ";
+}
+function myFunction3() { 
+    document.getElementById("demo3").innerHTML = "Esse é o site do projeto, de forma resumida realizada no Técnico ";
+}
+function myFunction4() { 
+    document.getElementById("demo4").innerHTML = "Essa é o MVP da solução do projeto realizado no técnico ";
+}
