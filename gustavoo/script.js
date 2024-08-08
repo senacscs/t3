41428e3e6da86511a30d4b2979a7be7c17250e3c
Gustavo Oliveira <132953994+Gu-oliveir@users.noreply.github.com>
2024-08-08 11:14:57 -0300

    delet

--- gustavoo/script.js
+++
@@ -1,6 +0,0 @@
-function myFunction() { 
-    document.getElementById("demo").innerHTML = "Todos trabalhos feito em 2023 por mim!";
-}
-function myFunction2() { 
-    document.getElementById("demo2").innerHTML = "Foi feito um site de explicação de back-end, com o mesmo intuito do primeiro site do ano passado ";
-}
