02705898ac151ccc2e3076178bbd7ac22346092f
Gustavo Oliveira <132953994+Gu-oliveir@users.noreply.github.com>
2024-09-16 08:43:25 -0300

    .

---
+++ gustavoo/segundo/projeto/script.js
@@ -0,0 +1,6 @@
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+    // Aqui você pode adicionar lógica para enviar o formulário por meio de uma requisição AJAX, se necessário.
+});
