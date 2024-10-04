93c5cc82b22f9e612c79bf84d1999ebd33e39e2e
EduLunardi0130 <132913101+EduLunardi0130@users.noreply.github.com>
2024-10-03 21:15:49 -0300

    Click do bem

---
+++ eduarda/segundo/aplicacao/doacoes/dinheiro.js
@@ -0,0 +1,25 @@
+$(".denomination").click(function(event) {
+    $(".denomination").removeClass("selected").prop('checked', false);
+    $(".denomination-other input").removeClass("selected").val('');
+    $(this).addClass("selected");
+    $(this).children(":first").prop('checked', true);
+    $("button").text('Donate $' + $(this).children(":first").val())
+  });
+  
+  $(".denomination-other input").on('keypress', function (event) {
+    // allow only int values
+    // TODO: remove leading 0
+    var regex = new RegExp("^[0-9]+$");
+    var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+    if (!regex.test(key)) {
+      event.preventDefault();
+      return false;
+    }
+    
+    $(".denomination").removeClass("selected").prop('checked', false);
+    $(this).addClass("selected");
+    $("button").text('Donate $' + $(this).val() + key );
+  });
+  
+  
+  
